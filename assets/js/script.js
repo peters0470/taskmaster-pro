bbb7cf2bb8caf197a4c757dd9fb6f28d67fa0e3b
unknown <peters0470@gmail.com>
2020-10-18 13:21:36 -0500

    changes to date features

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,8 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
-
+  auditTask(taskLi);
+    
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
 };
@@ -83,13 +84,18 @@ $(".list-group").on("click", "span", function() {
     .addClass("form-control")
     .val(date);
   $(this).replaceWith(dateInput);
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function() {
+      $(this).trigger("change");
+    }
+  });
   dateInput.trigger("focus");  
 });
 
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   var date = $(this)
-    .val()
-    .trim();
+    .val();
   var status = $(this)
     .closest(".list-group")
     .attr("id")
@@ -103,6 +109,7 @@ $(".list-group").on("blur", "input[type='text']", function() {
     .addClass("badge badge-primary badge-pill")
     .text(date);
   $(this).replaceWith(taskSpan);
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 $(".card .list-group").sortable({
@@ -160,6 +167,22 @@ $("#trash").droppable({
   }
 });
 
+$("#modalDueDate").datepicker({
+  minDate: 1
+});
+
+var auditTask = function(taskEl) {
+  var date = $(taskEl). find("span").text().trim();
+  var time = moment(date, "L").set("hour", 17);
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  }
+  else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+  
+};
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {

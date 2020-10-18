75389019bf69f43622b4b91d6e5862ad42b47344
unknown <peters0470@gmail.com>
2020-10-18 16:36:26 -0500

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -118,15 +118,21 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
 
   },
   deactivate: function(event) {
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
 
   },
   over: function(event) {
+    $(event.target).addClass("dropover-active");
 
   },
   out: function(event) {
+    $(event.target).removeClass("dropover-active");
 
   },
   update: function(event) {
@@ -160,10 +166,13 @@ $("#trash").droppable({
   tolerance: "touch", 
   drop: function(event, ui) {
     ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   over: function(event, ui) {
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
@@ -181,8 +190,13 @@ var auditTask = function(taskEl) {
   else if (Math.abs(moment().diff(time, "days")) <= 2) {
     $(taskEl).addClass("list-group-item-warning");
   }
-  
 };
+
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, 1800000);
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
@@ -197,7 +211,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
